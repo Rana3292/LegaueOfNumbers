38566fbaeca94ac9665dfe137aa0e30ae1119aa8
AnKa <Rana3292@googlemail.com>
2012-12-13 16:45:46 +0100

    Test.java mit Ausgabe
    Change-Id: Ic7403716ac4a0dc6c48d96bbe6e60a3149db2f34
    Signed-off-by: AnKa <Rana3292@googlemail.com>

---
+++ LoN/src/test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author kat
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("League of Numbers = LoN");
+
+	}
+
+}
