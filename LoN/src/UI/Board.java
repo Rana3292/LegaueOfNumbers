072ebc505a2dd616bbc47dc46b09007c9840d026
Rana <Rana3292@googlemail.com>
2013-01-14 19:19:02 +0100

    Button enabling is implemented.

--- LoN/src/UI/Board.java
+++ LoN/src/UI/Board.java
@@ -1,6 +1,8 @@
 package UI;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,8 +12,9 @@ import GameField.GameField;
 
 
 public class Board {
-	public Board( JPanel	panel ){
+	public Board( JPanel	panel){
 		this.panel = panel;
+		this.eventHandler = eventHandler;
 		reset();
 		panel.setSize(model.getWidth()*50, model.getHeight()*50);
 		panel.setLayout(new GridLayout(model.getWidth(), model.getHeight()));
@@ -20,16 +23,28 @@ public class Board {
 	public void reset(){
 		model = new GameField();
 		buttons = new JButton[model.getWidth()][model.getHeight()];
-		for (int i=0; i < model.getWidth(); i++){
-			for (int j=0; j < model.getHeight(); j++){
+		int i, j;
+		for (i=0; i < model.getWidth(); i++){
+			for (j=0; j < model.getHeight(); j++){
 				buttons[i][j] = new JButton(model.getValue(j, i) + "");
 				buttons[i][j].setSize(50, 50);
 				/* HowTo identify a Button: 
-				 * int Value / model.getWidth() = column
-				 * int Value % model.getWidth() = row 
+				 * int Value / model.getWidth() = row
+				 * int Value % model.getWidth() = column 
 				 */
 				buttons[i][j].setActionCommand(i*model.getWidth() + j + ""); 
 				panel.add(buttons[i][j]);
+				buttons[i][j].addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String s = e.getActionCommand();
+						int buttonid = Integer.parseInt(s);
+						int column = buttonid % getWidthGameField();
+						int row = buttonid / getWidthGameField();
+						enableButtons(row, column);
+					}
+				});
 			}
 		}
 	}
@@ -37,8 +52,31 @@ public class Board {
 	public void update(){
 		for (int i=0; i < model.getWidth(); i++){
 			for (int j=0; j < model.getHeight(); j++){
-				if (model.getStriked(i,j)){
+				if (model.getStriked(j,i)){
 					buttons[i][j].setEnabled(true);
+				}
+			}
+		}
+	}
+	
+	public void enableButtons(int x, int y){
+		for (int i=0; i < model.getWidth(); i++){
+			for (int j=0; j < model.getHeight(); j++){
+				buttons[i][j].setEnabled(false);
+				//Setting the Buttons around not enabled!
+				if (i == x-1 || i == x+1){ 
+					if ( j == y-1 || (j==y) || j == y+1){
+						if (! model.getStriked(j, i)){
+							buttons[i][j].setEnabled(true);
+						}
+					}
+				}
+				if (x == i){
+					if (j == y-1 || j == y+1){
+						if (! model.getStriked(j, i)){
+							buttons[i][j].setEnabled(true);
+						}
+					}
 				}
 			}
 		}
@@ -54,4 +92,5 @@ public class Board {
 	private JButton[][] buttons;
 	private GameField model;
 	private JPanel panel;
+	private EventHandleBoard eventHandler;
 }
--- LoN/src/UI/GamePanel.java
+++ LoN/src/UI/GamePanel.java
@@ -7,6 +7,8 @@ import javax.swing.WindowConstants;
 
 
 class GamePanel extends JFrame{
+
+
 	public GamePanel(){
 		super();
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -19,7 +21,7 @@ class GamePanel extends JFrame{
 	}
 	private JPanel boardPanel;
 	private Board board;
-	
+	private static final long serialVersionUID = -5533014914272850813L;
 	
 	public static void main(String[] args) {
 		new GamePanel();
