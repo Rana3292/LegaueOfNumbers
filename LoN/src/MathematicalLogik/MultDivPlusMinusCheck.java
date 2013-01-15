72eec3009fdda6b5713b19289b4e98611a89c07e
lerentis <lerentis@gmail.com>
2013-01-15 13:23:12 +0100

    check for div by Zero Exception

--- LoN/src/MathematicalLogik/MultDivPlusMinusCheck.java
+++ LoN/src/MathematicalLogik/MultDivPlusMinusCheck.java
@@ -9,9 +9,9 @@ public class MultDivPlusMinusCheck {
 			return true;
 		if (a*b + c == res || b*c + a == res || a*c + b == res)
 			return true;
-		if (a%b == 0 && a/b + c == res || b%a == 0 && b/a + c== res ||
-				b%c == 0 && b/c+ a == res || c%b == 0 && c/b + a == res ||
-				a%c == 0 && a/c + b == res || c%a == 0 && c/a + b == res)
+		if (b!= 0 && a%b == 0 && a/b + c == res || a != 0 && b%a == 0 && b/a + c== res ||
+			c != 0 && b%c == 0 && b/c+ a == res ||b !=0 && c%b == 0 && c/b + a == res ||
+			c != 0 && a%c == 0 && a/c + b == res || a != 0 && c%a == 0 && c/a + b == res)
 			return true;
 		if ((a+b)*c == res || (b+c)*a == res || (a+c)*b == res){
 			return true;
@@ -21,14 +21,14 @@ public class MultDivPlusMinusCheck {
 			(a-c)*b == res || (c-a)*b == res){
 			return true;
 		}
-		if ((a+b)%c == 0 && (a+b)/c == res || 
-				(b+c)%a == 0 && (b+c)/a == res || 
-				(a+c)%b ==0 && (a+c)/b == res){
+		if (c != 0 && (a+b)%c == 0 && (a+b)/c == res || 
+				a != 0 && (b+c)%a == 0 && (b+c)/a == res || 
+				b != 0 && (a+c)%b ==0 && (a+c)/b == res){
 			return true;
 		}
-		if ((a-b)%c == 0 && (a-b)/c == res || (b-a)%c == 0 && (b-a)/c == res ||
-				(b-c)%a == 0 && (b-c)/a == res || (c-b)%a == 0 && (c-b)/a == res ||
-				(a-c)%b == 0 && (a-c)/b == res || (c-a)%b == 0 && (c-a)/b == res){
+		if (c != 0 &&(a-b)%c == 0 && (a-b)/c == res ||c != 0 && (b-a)%c == 0 && (b-a)/c == res ||
+				a != 0 &&(b-c)%a == 0 && (b-c)/a == res ||a != 0 && (c-b)%a == 0 && (c-b)/a == res ||
+				b != 0 &&(a-c)%b == 0 && (a-c)/b == res ||b != 0 && (c-a)%b == 0 && (c-a)/b == res){
 			return true;
 		}
 		return false;
