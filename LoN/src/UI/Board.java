ba0f8bb271b68506ec6132f84d2d86cdd14fc8d3
Rana <Rana3292@googlemail.com>
2013-01-20 16:39:17 +0100

    Added function of searching valid random numbers

--- LoN/src/UI/Board.java
+++ LoN/src/UI/Board.java
@@ -3,6 +3,7 @@ package UI;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -10,13 +11,21 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import GameField.GameField;
+import MathematicalLogik.MultDivCheck;
 import MathematicalLogik.MultDivPlusMinusCheck;
+import MathematicalLogik.PlusMinusCheck;
 
 
 
 public class Board {
-	public Board( JPanel	panel){
+	public final static int PLUSMINUS = 1;
+	public final static int MULITDIV = 2;
+	public final static int PLUSMINUSMULTDIV = 3;
+	
+	public Board( JPanel	panel, int gameValue, int maxNumber){
 		this.panel = panel;
+		this.gameValue = gameValue;
+		this.maxNumber = maxNumber;
 		reset();
 		panel.setSize(model.getWidth()*50, model.getHeight()*50);
 		panel.setLayout(new GridLayout(model.getWidth(), model.getHeight()));
@@ -26,6 +35,7 @@ public class Board {
 		model = new GameField();
 		values = new JButton[3];
 		counterForValue = 0;
+		createNewNumber();
 		buttons = new JButton[model.getWidth()][model.getHeight()];
 		int i, j;
 		for (i=0; i < model.getWidth(); i++){
@@ -65,17 +75,34 @@ public class Board {
 								}
 								JDialog dialog = new JDialog();
 								dialog.setSize(200, 200);
-								if (MultDivPlusMinusCheck.check(intValues[0], intValues[1], intValues[2], 10)){
-									dialog.add(new JLabel("Right!"));
-									for (int i=0; i < 3; i++){
-										int buttonString = Integer.parseInt(values[i].getActionCommand());
-										column = buttonString % getWidthGameField();
-										row = buttonString / getWidthGameField();
-										setStriked(column, row);
+								boolean checked = false;
+								if (gameValue == PLUSMINUS){
+									if (PlusMinusCheck.check(intValues[0], intValues[1], intValues[2], actualSearchedNumber)){
+										dialog.add(new JLabel("Right!"));
+										striking();
+										checked = true;
+										
 									}
 								}
-								else{
+								if (gameValue == MULITDIV){
+									if (MultDivCheck.check(intValues[0], intValues[1], intValues[2], actualSearchedNumber)){
+										dialog.add(new JLabel("Right!"));
+										striking();
+										checked = true;
+									}
+								}
+								if (gameValue == PLUSMINUSMULTDIV){
+									if (MultDivPlusMinusCheck.check(intValues[0], intValues[1], intValues[2], actualSearchedNumber)){
+										dialog.add(new JLabel("Right!"));
+										striking();
+										checked = true;
+									}
+								}
+								if (!checked){
 									dialog.add(new JLabel("False!"));
+								}
+								else{
+									createNewNumber();
 								}
 								dialog.setVisible(true);
 								
@@ -129,6 +156,12 @@ public class Board {
 	public int getWidthGameField(){
 		return model.getWidth();
 	}
+	public int getActualSearchedNumber(){
+		return actualSearchedNumber;
+	}
+	public void createNewNumber(){
+		System.out.println("New generated number " + randomNumber());
+	}
 	
 	private int getValue(int i, int j){
 		return model.getValue(j, i);
@@ -136,7 +169,6 @@ public class Board {
 	private void setStriked(int i, int j){
 		model.setStriked(i, j);
 	}
-	
 	private boolean setValue(JButton value){
 		if (counterForValue < 2){
 			check = false;
@@ -161,8 +193,93 @@ public class Board {
 		return false;
 	}
 	
+	private void striking(){
+		for (int i=0; i < 3; i++){
+			int buttonString = Integer.parseInt(values[i].getActionCommand());
+			int column = buttonString % getWidthGameField();
+			int row = buttonString / getWidthGameField();
+			setStriked(column, row);
+		}
+	}
+	
+	private int randomNumber(){
+		Random r = new Random();
+		int ret = 0;
+		int max = 0;
+		if (gameValue == PLUSMINUS){
+			max = 3 * maxNumber;
+		}
+		if (gameValue == MULITDIV || gameValue == PLUSMINUSMULTDIV){
+			max = maxNumber * maxNumber * maxNumber;
+		}
+		ret = r.nextInt(max);
+		while (! controllRandomNumber(ret)){
+			ret = r.nextInt(max);
+		}
+		actualSearchedNumber = ret;
+		return ret;
+	}
+	private boolean controllRandomNumber(int random){
+		for (int i=0; i < model.getHeight(); i++){
+			for (int j=0; j < model.getWidth(); j++){
+				if (i+2 < model.getWidth()){
+					if (gameValue == PLUSMINUS){
+						if (PlusMinusCheck.check(getValue(i,j),getValue(i+1,j),getValue(i+2,j), random)){
+							return true;
+						}
+					}
+					if (gameValue == MULITDIV){
+						if (MultDivCheck.check(getValue(i,j), getValue(i+1,j), getValue(i+2,j), random))
+							return true;
+					}
+					if (gameValue == PLUSMINUSMULTDIV){
+						if (MultDivPlusMinusCheck.check(getValue(i,j), getValue(i+1,j), getValue(i+2, j), random))
+							return true;
+					}
+				}
+				if (j+2 < model.getHeight()){
+					switch (gameValue){
+					case PLUSMINUS: if (PlusMinusCheck.check(getValue(i,j), getValue(i,j+1), getValue(i, j+2), random))
+						return true;
+						break;
+					case MULITDIV: if (MultDivCheck.check(getValue(i,j), getValue(i,j+1), getValue(i, j+2), random))
+						return true;
+						break;
+					case PLUSMINUSMULTDIV: if (MultDivPlusMinusCheck.check(getValue(i,j), getValue(i, j+1), getValue(i, j+2), random))
+						return true;
+						break;
+					}
+				}
+				if (j+1 < model.getHeight() && i+1 < model.getWidth()){
+					switch (gameValue){
+					case PLUSMINUS: if (PlusMinusCheck.check(getValue(i,j), getValue(i+1, j), getValue(i+1,j+1), random))
+						return true;
+						if (PlusMinusCheck.check(getValue(i,j), getValue(i, j+1), getValue(i+1, j), random))
+							return true;
+						break;
+					case MULITDIV: if (MultDivCheck.check(getValue(i,j), getValue(i+1, j), getValue(i+1, j+1), random))
+						return true;
+						if (MultDivCheck.check(getValue(i,j), getValue(i+1, j), getValue(i+1, j+1), random))
+							return true;
+						break;
+					case PLUSMINUSMULTDIV: if (MultDivPlusMinusCheck.check(getValue(i,j),getValue(i+1, j), getValue(i+1, j+1), random))
+							return true;
+						if (MultDivPlusMinusCheck.check(getValue(i,j), getValue(i+1,j), getValue(i+1, j+1), random))
+							return true;
+						break;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	
 	private boolean check; 
 	private int counterForValue;
+	private int gameValue;
+	private int maxNumber;
+	private int actualSearchedNumber;
 	private JButton[] values;
 	private JButton[][] buttons;
 	private GameField model;
