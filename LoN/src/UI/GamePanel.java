38362b81d5eb7d06e2b2e2575411cb2bddf7672c
Rana <Rana3292@googlemail.com>
2013-01-20 16:39:52 +0100

    Added function of searching valid random numbers

--- LoN/src/UI/GamePanel.java
+++ LoN/src/UI/GamePanel.java
@@ -1,5 +1,6 @@
 package UI;
 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
@@ -13,14 +14,18 @@ class GamePanel extends JFrame{
 		super();
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
+		/*JDialog dialog = new JDialog();
+		dialog.add(new JLabel("You have choosen three values :)"));
+		dialog.setSize(200, 200);
+		dialog.setVisible(true);*/
+		
 		boardPanel = new JPanel();
-		board = new Board(boardPanel);
+		new Board(boardPanel, Board.PLUSMINUS, 10);
 		this.add(boardPanel);
 		this.setSize(600, 600);
 		this.setVisible(true);
 	}
 	private JPanel boardPanel;
-	private Board board;
 	private static final long serialVersionUID = -5533014914272850813L;
 	
 	public static void main(String[] args) {
