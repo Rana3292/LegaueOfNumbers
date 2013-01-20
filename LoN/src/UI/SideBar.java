4b23147507373a6cfbf27aae99a39c2b3f61ca3c
Rana <Rana3292@googlemail.com>
2013-01-20 23:47:34 +0100

    Two Player mode is implemented and new images are added

--- LoN/src/UI/SideBar.java
+++ LoN/src/UI/SideBar.java
@@ -7,10 +7,13 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -21,16 +24,41 @@ import javax.swing.JPanel;
  */
 public class SideBar implements Observer {
 	public SideBar(JPanel panel, Board board){
+		pointsPlayerA =0;
+		pointsPlayerB = 0;
+		movePlayer = -1;
 		this.panel = panel;
-		panel.setLayout(new GridLayout(2,1));
+		panel.setLayout(new GridLayout(3,1));
 		this.board = board;
 		searchNumber= new JLabel(board.getActualSearchedNumber() + "");
 		lValidNumber = new JLabel();
 		lValidNumber.setIcon(new ImageIcon("richtig.jpg"));
 		
-
+		pPlayers = new JPanel(new GridLayout(3,1));
+		lPlayerPoints = new JLabel("Player A: " + pointsPlayerA + " | Player B: " + pointsPlayerB);
+		bPlayerA = new JButton("Player A");
+		bPlayerA.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				movePlayer = moveByA;
+			}
+		});
+		bPlayerB = new JButton("Player B");
+		bPlayerB.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				movePlayer = moveByB;
+			}
+		});
+		pPlayers.add(bPlayerA);
+		pPlayers.add(bPlayerB);
+		pPlayers.add(lPlayerPoints);
+		pPlayers.setVisible(true);
 		panel.add(searchNumber);
 		panel.add(lValidNumber);
+		panel.add(pPlayers);
 
 		
 		panel.setMinimumSize(new Dimension(150, board.getHeightGameField()*50));
@@ -45,16 +73,38 @@ public class SideBar implements Observer {
 		searchNumber.setText(arg+"");
 		
 		if (board.getValidAnswer()){
-			lValidNumber.setIcon(new ImageIcon("richtig.jpg"));
+			switch(movePlayer){
+			case moveByA: pointsPlayerA++;
+				break;
+			case moveByB: pointsPlayerB++;
+				break;
+			}
+			lValidNumber.setIcon(new ImageIcon("richtig.png"));
 		}
 		else{
-			lValidNumber.setIcon(new ImageIcon("falsch.jpg"));
+			switch(movePlayer){
+			case moveByA: pointsPlayerB++;
+				break;
+			case moveByB: pointsPlayerA++;
+				break;
+			}
+			lValidNumber.setIcon(new ImageIcon("falsch.png"));
 		}
+		movePlayer = -1;
+		lPlayerPoints.setText("Player A: " + pointsPlayerA + " | Player B: " + pointsPlayerB);
 	}
 	
 	private JPanel panel;
 	private Board board;
 	private JLabel searchNumber;
 	private JLabel lValidNumber;
-
+	private int pointsPlayerA;
+	private int pointsPlayerB;
+	private JPanel pPlayers;
+	private JLabel lPlayerPoints;
+	private JButton bPlayerA;
+	private JButton bPlayerB;
+	private int movePlayer;
+	private static final int moveByA = 0;
+	private static final int moveByB = 1;
 }
